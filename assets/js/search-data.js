cf12a63264341fdc4a219c64bc1f6d50135f1443
davesuonabene <davesuonabene@gmail.com>
2026-01-27 11:31:12 +0000

    Deploying to gh-pages from @ davesuonabene/davesuonabene.github.io@75e489e911fe8f2238e34899d6455287d4538364 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,15 +3,15 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-home",
+    title: "home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "Se davvero ti serve, ecco il mio CV",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -179,17 +179,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2023/post-bibliography/";
-          
-        },
-      },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/jupyter-notebook/";
           
         },
       },{id: "post-a-post-with-custom-blockquotes",
